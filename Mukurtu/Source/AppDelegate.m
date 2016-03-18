072110f779a8fd7f5752ee2183b1e37b4b6a50e5
magillo <magillo@Clifton.clearos2012.net.ikm>
2016-03-18 17:12:33 +0100

    cleanup AppDelegate

--- Mukurtu/Source/AppDelegate.m
+++ Mukurtu/Source/AppDelegate.m
@@ -47,7 +47,7 @@
     
     DLog(@"App Major Version 2.5");
     
-    //FIX 2.5: store Major version lock to remove DB in old versions at first launch
+    //store Major version lock to remove DB in old versions at first launch
     PDKeychainBindings *bindings=[PDKeychainBindings sharedKeychainBindings];
     NSString *appVersion = [[bindings objectForKey:kMukurtuAccountKeychainAppMajorVersion] copy];
     
@@ -92,19 +92,13 @@
         [defaults setObject:[NSNumber numberWithBool:NO] forKey:kMukurtuStoredLoggedInStatus];
         [defaults synchronize];
         
-        //FIXME
-        //remove all documents files here
-        
+        //TODO: could remove all documents files here
     }
     
     DLog(@"Setup Core Data stack");
     [MagicalRecord setupCoreDataStack];
-        
-    //[MagicalRecord setupAutoMigratingCoreDataStack];
     
     MukurtuSession *sharedSession = [MukurtuSession sharedSession];
-    
-    DLog(@"\n\n\n\nIs there anybody out there?\n\n\n\n\n");
     
     if (!sharedSession.userIsLoggedIn)
     {
@@ -120,30 +114,22 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Saves changes in the application's managed object context before the application terminates.
-    //[self saveContext];
 }
 
 
